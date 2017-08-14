9595526f515be937b2c70fe28d10a49fa811079f
Katherine Wu <katwu90@gmail.com>
2017-08-13 22:10:07 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -29,15 +29,24 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
   }
 };
 
-var flipCard = function(cardId){
+var flipCard = function(){
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+this.setAttribute('src', cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId.rank]);
 if (cardsInPlay.length === 2) {
 checkForMatch();
 }
 };
 
-flipCard(0)
-flipCard(1)
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
